03bdc44c8949ee7f4b376570135a1319cce5f7bd
Ordenador Eve <evelyndayanara.vanegas@unavarra.es>
2024-04-12 13:16:00 +0200

    MisPedidos Modificados

--- my_webapp_2024/src/Paginas/MisPedidos.js
+++ my_webapp_2024/src/Paginas/MisPedidos.js
@@ -1,36 +1,41 @@
-import './MisPedidos.css'
+import './MisPedidos.css';
 import Table from 'react-bootstrap/Table';
 import Card from 'react-bootstrap/Card';
 
 const MisPedidos = () => {
-
     const pedidos = [
         { id: 1, producto: 'Producto 1', cantidad: 3, precioUnitario: 10 },
         { id: 2, producto: 'Producto 2', cantidad: 2, precioUnitario: 15 },
         { id: 3, producto: 'Producto 3', cantidad: 1, precioUnitario: 20 },
+        { id: 1, producto: 'Producto 1', cantidad: 3, precioUnitario: 10 },
+        { id: 2, producto: 'Producto 2', cantidad: 2, precioUnitario: 15 },
+        { id: 3, producto: 'Producto 3', cantidad: 1, precioUnitario: 20 },
+        { id: 1, producto: 'Producto 1', cantidad: 3, precioUnitario: 10 },
+        { id: 2, producto: 'Producto 2', cantidad: 2, precioUnitario: 15 },
+        { id: 3, producto: 'Producto 3', cantidad: 1, precioUnitario: 20 },
+        { id: 1, producto: 'Producto 1', cantidad: 3, precioUnitario: 10 },
+        { id: 2, producto: 'Producto 2', cantidad: 2, precioUnitario: 15 },
+        { id: 3, producto: 'Producto 3', cantidad: 1, precioUnitario: 20 },
+        { id: 1, producto: 'Producto 1', cantidad: 3, precioUnitario: 10 },
+        { id: 3, producto: 'Producto 3', cantidad: 1, precioUnitario: 20 },
     ];
 
-
     return (
-        <Card
-          bg={'Dark'.toLowerCase()}
-          key={'Dark'}
-          text={'Dark'.toLowerCase() === 'light' ? 'dark' : 'white'}
-          className="mb-2"
-        >
-            <Card.Body>
-                <Card.Title>MIS PEDIDOS</Card.Title>
-                <Card.Text>
+        <div className="d-flex justify-content-center align-items-center h-100">
+            <Card className="custom-card">
+                <Card.Body>
+                    <Card.Title className="custom-title">MIS PEDIDOS</Card.Title>
                     <Table responsive="sm">
-                        <thead>
+                        <thead className="custom-thead">
                             <tr>
-                                <th></th>
-                                <th>Producto</th>
-                                <th>Cantidad</th>
-                                <th>Precio Unitario</th>
-                                <th>Total</th>
+                                <th className="custom-th"></th>
+                                <th className="custom-th">Producto</th>
+                                <th className="custom-th">Cantidad</th>
+                                <th className="custom-th">Precio Unitario</th>
+                                <th className="custom-th">Total</th>
                             </tr>
                         </thead>
+                        
                         <tbody>
                             {pedidos.map((pedido, index) => (
                                 <tr key={index}>
@@ -43,9 +48,9 @@ const MisPedidos = () => {
                             ))}
                         </tbody>
                     </Table>
-                </Card.Text>
-            </Card.Body>
-        </Card>
+                </Card.Body>
+            </Card>
+        </div>
     );
 }
 
